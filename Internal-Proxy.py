b35f8174c34e8e8e15569d00be5f763f0e2c97f0
jdye64 <jdye64@gmail.com>
2014-04-17 09:41:02 -0400

    Created installation and debian service files for Raspberry PI installation

--- Internal-Proxy.py
+++ Internal-Proxy.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # This is a simple port-forward / proxy, written using only the default python
 # library.
 
---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+echo 'Installing Python Internal Network Proxy'
+cp ./Internal-Proxy.py /usr/local/bin/.
+mkdir /usr/local/bin/python-proxy
+chmod 755 /usr/local/bin/python-proxy
+chmod 755 /usr/local/bin/python-proxy/Internal-Proxy.py
+
+# Move the init script to /etc/init.d
+cp ./pythonproxy.sh /etc/init.d/.
+chmod 755 /etc/init.d/pythonproxy.sh
+
+echo 'Updating etc inti defaults for pythonproxy'
+update-rc.d pythonproxy.sh defaults
+
+echo 'Python Internal Network Proxy has been installed'
+
+
